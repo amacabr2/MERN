937a7b70943143a3a05cd445481140c870182b68
amacabr2 <anthony.macabrey70@gmail.com>
2019-02-27 14:33:10 +0100

    Login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput, MDBCard, MDBCardBody } from 'mdbreact';
+import API from '../utils/API';
+
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email : "",
+            password: ""
+        }
+        this.handleChange.bind(this);
+        this.send.bind(this);
+    }
+
+    send = event => {
+        if(this.state.email.length === 0){
+            return;
+        }
+
+        if(this.state.password.length === 0){
+            return;
+        }
+
+        API.login(this.state.email, this.state.password).then(function(data){
+            localStorage.setItem('token', data.data.token);
+            window.location = "/dashboard"
+        },function(error){
+            console.log(error);
+            return;
+        })
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    render() {
+        return(
+            <MDBContainer>
+                <MDBRow center>
+                    <MDBCol>
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBInput controlId="email" label="Email" autoFocus type="email" value={this.state.email} onChange={this.handleChange} outline />
+                                <MDBInput controlId="password" label="Example label" type="password" value={this.state.password} onChange={this.handleChange} outline />
+                                <MDBBtn color="" onClick={this.send}>Connexion</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        )
+    }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,10 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
