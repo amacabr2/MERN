0989261f1fc2b0d32b4d69737b888cdeef085c99
amacabr2 <anthony.macabrey70@gmail.com>
2019-02-27 13:38:04 +0100

    Identification and connection of a user (backend code)

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const bodyParser = require('body-parser');
 //On définit notre objet express nommé app
 const app = express();
 
+//Connexion à la base de donnée
+mongoose.connect('mongodb://localhost/db').then(() => {
+    console.log('Connected to mongoDB')
+}).catch(e => {
+    console.log('Error while DB connecting');
+    console.log(e);
+});
+
 //Body Parser
 let urlencodedParser = bodyParser.urlencoded({
     extended: true
@@ -22,10 +30,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-//On définit la route Hello
-app.get('/hello',function(req,res){
-    res.json("Hello World")
-})
+//Définition du routeur
+let router = express.Router();
+app.use('/user', router);
+require(__dirname + '/controllers/UserController')(router);
 
 //Définition et mise en place du port d'écoute
 let port = 8000;
